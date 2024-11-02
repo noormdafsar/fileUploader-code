d1afba2866d87923ea2eac97b1c4476bb9a10900
noormdafsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2024-11-02 18:45:00 +0530

    nodemailer config code added and added code for sending mail

--- models/file.js
+++ models/file.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+//const nodemailer = require('nodemailer');
+const transporter = require('../config/nodemailer');
 
 const fileSchema = new Schema({
     name: {
@@ -26,5 +28,25 @@ const fileSchema = new Schema({
 }, { 
     timestamps: true
 });
+
+// Post middleware
+fileSchema.post('save', async function(doc) {
+    try {
+        console.log('DOCs', doc);
+        // transporter imported from conifg/nodemailer.js
+        let info = await transporter.sendMail({
+            from: process.env.MAIL_USER,
+            to: doc.email,
+            subject: 'File uploaded successfully',
+            text: 'Your file has been uploaded successfully',
+            html: `<h1>Your file has been uploaded successfully</h1> View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a>`,
+        });
+        console.log('Message sent: %s', info);
+    }
+    catch(err) {
+        console.log('Error while sending mail', err);
+    }
+
+})
 
 module.exports = mongoose.model('File', fileSchema); 
